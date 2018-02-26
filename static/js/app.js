3fcea6259eef6eaee02158f1a2d43ec21e02d134
Felipe Zipitría <fzipitria@perceptyx.com>
2018-02-25 23:54:03 -0300

    Changed table responsive layout, and adding humanize to table head

--- static/js/app.js
+++ static/js/app.js
@@ -30,7 +30,7 @@ $.grayLightest =  '#f8f9fa';
 $(document).ready(function($){
 
   // Add table class to markdown tables
-  $('.main table:not(.table)').addClass('table table-bordered table-responsive').css({backgroundColor: 'white'}).find('td').css( { padding: '5px' })
+  $('.main table:not(.table)').addClass('table table-bordered table-responsive-sm table-hover table-outline mb-0').css({backgroundColor: 'white'}).find('td').css( { padding: '5px' })
 
   // Add class .active to current link
   $.navigation.find('a').each(function(){
